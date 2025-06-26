cfb092d87cc96a7d149cab2b5759ca3e843f6825
Shakila Zumur <shakilazumur@gmail.com>
2025-06-26 22:47:29 +0300

    added function field

--- js/utilities.js
+++ js/utilities.js
@@ -1,8 +1,15 @@
 // console.log('utilities file added');
 // Common shared functions here
 
-console.log('utilities loaded')
+// console.log('utilities loaded')
 
-function getInputFieldValueById(){
-    console.log('will get value by id');
+// function getInputFieldValueById(){
+//     console.log('will get value by id');
+//     const addMoney = document.getElementById('input-add-money').value;
+//     return addMoney;
+// }
+
+function getInputFieldValueById(id){
+    const inputValue = document.getElementById(id).value;
+    return inputValue;
 }
