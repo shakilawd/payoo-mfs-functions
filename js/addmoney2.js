15d1a7094aa7df11d7e5eec823ae3749d5213ad6
Shakila Zumur <shakilazumur@gmail.com>
2025-06-28 22:33:36 +0300

    added cashout.js files

--- js/addmoney2.js
+++ js/addmoney2.js
@@ -2,14 +2,18 @@ document.getElementById('btn-add-money')
     .addEventListener('click', function (event) {
         event.preventDefault();
 
-        const addMoney = getInputFieldValueById('input-add-money')
-        const pinNumber = getInputFieldValueById('input-pin-number')
+        const addMoney = getInputFieldValueById('input-add-money');
+        const pinNumber = getInputFieldValueById('input-pin-number');
 
         // console.log('add money inside addMoney2.js', addMoney, pinNumber)
         
         // wrong way to varify.do not try it at your serious websides
         if(pinNumber === 1234){
-           
+           const balance = getTextFieldValueById('account-balance');
+        //    console.log(balance, addMoney);
+        const newBalance = balance + addMoney;
+
+        document.getElementById('account-balance').innerText = newBalance;
         }
          else{
                 alert('Failed to add money.')
--- js/utilities2.js
+++ js/utilities2.js
@@ -1,5 +1,10 @@
 function getInputFieldValueById(id){
     const inputValue = document.getElementById(id).value;
     const inputNumber = parseFloat(inputValue);
-    return inputValue;
+    return inputNumber;
+}
+function getTextFieldValueById(id){
+    const textValue = document.getElementById(id).innerText;
+    const textNumber = parseFloat(textValue);
+    return textNumber;
 }
