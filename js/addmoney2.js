096fc1dc274764a35bd4846c1b40c39760a77fb5
Shakila Zumur <shakilazumur@gmail.com>
2025-06-30 22:04:55 +0300

    added the function, document

--- js/addmoney2.js
+++ js/addmoney2.js
@@ -4,6 +4,10 @@ document.getElementById('btn-add-money')
 
         const addMoney = getInputFieldValueById('input-add-money');
         const pinNumber = getInputFieldValueById('input-pin-number');
+        if(isNaN(addMoney)){
+            alert('Failed to add money');
+            return;
+        }
 
         // console.log('add money inside addMoney2.js', addMoney, pinNumber)
         
@@ -14,6 +18,14 @@ document.getElementById('btn-add-money')
         const newBalance = balance + addMoney;
 
         document.getElementById('account-balance').innerText = newBalance;
+
+        // add to transaction history
+        const p = document.createElement('p');
+        p.innerText = `Added: ${addMoney} Tk. Balance: ${newBalance}`;
+        console.log(p);
+        // showld  be a common function
+        document.getElementById('transaction-container').appendChild(p);
+
         }
          else{
                 alert('Failed to add money.')
